dc6f549018cffb982a5c4a260710033a986d7f47
potatoSalad21 <eeperpeepo@gmail.com>
2025-01-28 20:29:05 +0400

    CHANGE: autocmd for opening file browser for vim <directory path>

--- .config/nvim/lua/nomouse/init.lua
+++ .config/nvim/lua/nomouse/init.lua
@@ -9,6 +9,33 @@ local nomouseGroup = augroup("nomouse", {})
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
+-- make oil.nvim open a file browser when you type in a directory path
+autocmd("BufEnter", {
+    desc = string.format("[%s] replacement for Netrw", plugin_name),
+    pattern = "*",
+    callback = function()
+        vim.schedule(function()
+            local buffer_name = vim.api.nvim_buf_get_name(0)
+            if vim.fn.isdirectory(buffer_name) == 0 then
+                _, previous_buffer_name = pcall(vim.fn.expand, "#:p:h")
+                return
+            end
+
+            -- Avoid reopening when exiting without selecting a file
+            if previous_buffer_name == buffer_name then
+                previous_buffer_name = nil
+                return
+            else
+                previous_buffer_name = buffer_name
+            end
+
+            -- Ensure no buffers remain with the directory name
+            vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = 0 })
+            plugin_open(vim.fn.expand("%:p:h"))
+        end)
+    end,
+})
+
 autocmd("LspAttach", {
     group = nomouseGroup,
     callback = function(e)
