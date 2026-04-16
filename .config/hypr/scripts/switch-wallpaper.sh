fa501b623ffa5e235b5e8885aa168cbc6f00eadf
potatoSalad21 <datuna.ziraqishvili@gmail.com>
2026-04-16 13:00:00 +0400

    updated stuff from the older version

--- .config/hypr/scripts/switch-wallpaper.sh
+++ .config/hypr/scripts/switch-wallpaper.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # check if the daemon is running
-PROCESS_COUNT=$(ps -ef | grep -v grep | grep -cw swww-daemon)
+PROCESS_COUNT=$(ps -ef | grep -v grep | grep -cw awww-daemon)
 
 if [ $PROCESS_COUNT -eq 0 ]; then
-    echo ":: swww daemon not running"
-    echo ":: starting the swww daemon"
-    swww-daemon &
+    echo ":: awww daemon not running"
+    echo ":: starting the awww daemon"
+    awww-daemon &
 fi
 
-swww img $1 && wal -i $1
+awww img $1 && wal -i $1
